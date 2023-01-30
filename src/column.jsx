4c523220393c51fc7fa51b141589c0fa9739472a
Downtown2-6 <t.fares85@gmail.com>
2023-01-30 16:19:21 -0500

    tasks droppable between columns/lists. Investigate rolling back to React 17 for issues with library

--- src/column.jsx
+++ src/column.jsx
@@ -6,13 +6,20 @@ import Task from './task'
 const Container = styled.div`
   margin: 8px;
   border: 1px solid lightgrey;
-  border-radius: 2px;`
+  border-radius: 2px;
+  width: 220px;
+  display: flex;
+  flex-direction: column;`
+
 const Title = styled.h3`
   padding: 8px`
+
 const TaskList = styled.div`
   padding: 8px
   transition: background-color 0.2s ease;
-  background-color: ${props => (props.isDraggingOver ? 'skyblue' : 'white')}`
+  background-color: ${props => (props.isDraggingOver ? 'skyblue' : 'white')}
+  flex-grow: 1;
+  min-height: 100px;`
 
 
 export default class Column extends React.Component {
@@ -24,7 +31,7 @@ export default class Column extends React.Component {
           {(provided, snapshot) => (
             <TaskList
                 ref={provided.innerRef}
-                // innerRef={provided.innerRef}
+                innerRef={provided.innerRef}
                 {...provided.droppableProps}
                 isDraggingOver={snapshot.isDraggingOver}
               >
